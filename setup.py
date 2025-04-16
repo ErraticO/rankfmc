3b100bd4a7157b853ca6d587020200c6658adcdf
WangYunHua-TREC <10154231wang_yunhua@cn.tre-inc.com>
2025-04-16 14:29:25 +0800

    Fix pipy

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import numpy
 from setuptools import Extension, setup
 
 NAME = 'rankfmc'
-VERSION = '0.3.2'
+VERSION = '0.3.3'
 
 # define the extension packages to include
 # ----------------------------------------
